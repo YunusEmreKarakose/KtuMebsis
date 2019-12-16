531d430cf7db176efc07ade8eba6471a454d2adf
Yunus Emre Karaköse <yemre05@hotmail.com.tr>
2019-12-16 17:15:37 +0300

    routes

--- routes/companies.js
+++ routes/companies.js
@@ -4,6 +4,19 @@ const mysql=require('./database');
 
 /* GET home page. */
 router.post('/createComp', function(req, res, next) {
+  let post={
+    name:req.body.name,
+    phone:req.body.phone,
+    adress:req.body.adress,
+    info:req.body.info
+  };
+  
+  let postquery='INSERT INTO companies SET ?';
+  mysql.query(postquery,post, function(err, results, fields) {
+    if (err) {
+    console.log(err.message);
+    }
+  });
   res.render('index', { title: 'Express' });
 });
 
--- routes/ex.js
+++ routes/ex.js
@@ -3,7 +3,20 @@ var router = express.Router();
 const mysql=require('./database');
 
 /* GET home page. */
-router.post('/createComp', function(req, res, next) {
+router.post('/createEx', function(req, res, next) {
+  let post={
+    cId:req.body.cId,
+    userId:req.body.userId,
+    startyear:req.body.startyear,
+    endyear:req.body.endyear
+  };
+  
+  let postquery='INSERT INTO ex SET ?';
+  mysql.query(postquery,post, function(err, results, fields) {
+    if (err) {
+    console.log(err.message);
+    }
+  });
   res.render('index', { title: 'Express' });
 });
 
