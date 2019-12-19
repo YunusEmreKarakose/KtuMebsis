dd90d8dd886a981cb994fde4387a826c458437f4
Yunus Emre Karaköse <yemre05@hotmail.com.tr>
2019-12-19 23:30:00 +0300

    önyüz+ anasayfa işlemleri

--- routes/posts.js
+++ routes/posts.js
@@ -25,5 +25,52 @@ router.post('/post', function(req, res, next) {
     res.redirect('/');
   }
 });
+/*Paylaşımları getir*/
+router.get('/get', function(req, res, next) { 
+  if(req.session.idNumber){ 
+    
+    let getquery='SELECT * FROM posts';
+    mysql.query(getquery, function(err, result, fields) {
+      if (err) {
+      console.log(err.message);
+      }else{
+        var table='';
+        var i=0;
+        result.forEach(element => {
+          table +='<tr><td>'+ result[i].pId  +'</td><td>'+ result[i].senderId  +'</td><td>'+ result[i].type +'</td><td>'+result[i].duration+'</td><td>'+ result[i].company +'</td><td>'+ result[i].period +'</td><td>'+ result[i].info +'</td></tr>';
+          i++;
+        });
+        table ='<table border="1"></th><th>Post ID</th></th><th>Sender ID</th><th>Tip</th><th>Süre</th><th>Şirket</th><th>Periyot</th><th>Bilgi</th></tr>'+ table +'</table>';
+        res.send(table);
+      }
+    });
+  }else{
+   res.redirect('/');
+  }
+});
+/*Paylaşımları getir*/
+router.get('/most', function(req, res, next) { 
+  if(req.session.idNumber){ 
+
+    let getquery='SELECT COUNT(senderId),senderId AS MOST FROM posts GROUP BY senderId ORDER BY senderId DESC';
+    mysql.query(getquery, function(err, result, fields) {
+      if (err) {
+      console.log(err.message);
+      }else{
+        var most=result[0].MOST
+        let mmmm='SELECT * FROM users WHERE idNumber=?';
+          mysql.query(mmmm,most ,function(err, result, fields) {
+            if (err) {
+            console.log(err.message);
+            }else{
+                res.send(result[0].name+" "+result[0].surname+"  "+result[0].stuNum);
+            }
+          });
+      }
+    });
+  }else{
+   res.redirect('/');
+  }
+});
 
 module.exports = router;
